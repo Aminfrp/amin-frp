c323af7fdb7bee1b6623a625d68ef34f2f17ac2a
a.firouzpour <a.firouzpour@novin.dev>
2022-07-11 09:30:01 +0430

    fix: :bug: fix extera skills icons

--- src/components/layouts/sidebar/exteraSkills/Items.tsx
+++ src/components/layouts/sidebar/exteraSkills/Items.tsx
@@ -5,7 +5,12 @@ const Items = ({  name }: { name: string }) => {
     <>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
         <Typography variant="body1" fontSize={15} color="#767676">
-        <img src={"https://www.amin-frp.ir/icons.svg"} alt="extera" style={{marginRight:"15px"}}/>
+        <svg width="15" height="15" style={{marginRight:"15px"}} viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect x="0.5" y="0.5" width="9" height="9" stroke="#FFB400"/>
+          <rect x="5.5" y="5.5" width="9" height="9" stroke="#FFB400"/>
+          <rect x="0.5" y="0.5" width="9" height="9" stroke="#FFB400"/>
+          <rect x="5.5" y="5.5" width="9" height="9" stroke="#FFB400"/>
+        </svg>
           {name}
         </Typography>
       </div>
